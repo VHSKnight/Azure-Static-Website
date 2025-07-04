7b97d0474a5d991cba54aed9ff4a39ef1a9f8a9a
Adam Tobin <adamleetobin@hotmail.co.uk>
2025-07-04 11:47:56 +0100

    Delete My-Static-App/api/hello/__init__.py

--- My-Static-App/api/hello/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Hello function called.')
-    return func.HttpResponse("Hello, world!", status_code=200)
